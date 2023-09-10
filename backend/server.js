bac23af460e9be56658c06b21dac9a9667627935
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-10 15:50:55 +0530

    models and error handler added

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import products from "./data/products.js";
 dotenv.config();
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
+connectDB();
+
 const app = express();
-app.get(cors());
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use("/api/products", productRoutes);
+app.use(errorHandler);
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
